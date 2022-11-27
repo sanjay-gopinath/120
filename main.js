2a0da2a776f9d20e5a20832791fccce2f053623b
sanjay-gopinath <101658536+sanjay-gopinath@users.noreply.github.com>
2022-11-27 21:47:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+Webcam.set({
+    width: 350,
+    height: 350,
+    image_format: 'png',
+    png_quality: 90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+function take_snapshot() {
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="' + data_uri + '"/>';
+    });
+}
+
+console.log('ml5 version:', ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/5mxzZHkpx/model.json', modelLoaded);
+
+function modelLoaded() {
+    console.log('Model Loaded');
+}
+
+function speak() {
+    var synth = window.speechSynthesis;
+    speak_data_1 = "The first prediction is " + prediction_1;
+    speak_data_2 = "And the second prediction is " + prediction_2;
+    var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
+    synth.speak(utterThis);
+}
